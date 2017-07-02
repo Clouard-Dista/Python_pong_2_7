22233deda635f01eb91d26345e5b54d429672d99
Talloir Vincent <vincenttall@hotmail.fr>
2017-07-02 14:53:54 +0200

    finish the game

--- classs.py
+++ classs.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 
 class Playeur:
@@ -7,6 +8,7 @@ class Playeur:
 		if typeP == 1:
 			self.y=10
 			self.x = 10
+			self.bot = False
 			self.sprite = pygame.image.load("yellowPlayeur.png").convert_alpha()
 		elif typeP == 2:
 			self.y=10
@@ -14,6 +16,7 @@ class Playeur:
 			self.sprite = pygame.image.load("bluePlayeur.png").convert_alpha()
 		self.height = 90
 		self.width = 45
+		self.score=0
 		self.sprite = pygame.transform.scale(self.sprite, (self.width,self.height))
 	
 	def move(self, direction):
@@ -25,4 +28,51 @@ class Playeur:
 	def moveKeyboard(self, key):
 		if self.y + key*10 <= 500-self.height and self.y + key*10 >= 0:
 			self.y = self.y + key*10
-		print(self.x,self.y)
+
+class Ball:
+
+	def __init__(self,x,y):
+		global pos, vel
+		self.height = 30
+		self.width = 30
+		self.x = x - self.width/2
+		self.y = y - self.height/2
+		self.vel_x = random.randrange(-3,3)
+		if self.vel_x == 0:
+			self.vel_x = 1;
+		self.vel_y = -random.randrange(-2,2)
+		if self.vel_y == 0:
+			self.vel_y = 1;
+		self.sprite = pygame.image.load("ball.png").convert_alpha()
+		self.sprite = pygame.transform.scale(self.sprite, (self.width,self.height))
+
+	def move(self,playeur1,playeur2):
+		if self.y <= 0:
+			self.vel_y = - self.vel_y
+		if self.y >= 500 - self.height:
+			self.vel_y = -self.vel_y
+
+		if playeur1.x < self.x + self.width and playeur1.x + playeur1.width > self.x and playeur1.y < self.y + self.height and playeur1.height + playeur1.y > self.y or playeur2.x < self.x + self.width and playeur2.x + playeur2.width > self.x and playeur2.y < self.y + self.height and playeur2.height + playeur2.y > self.y:
+			self.vel_x = -self.vel_x
+			self.vel_x *= 1.1
+			self.vel_y *= 1.1
+		
+		if self.x < -self.width:
+			return 1
+		if self.x > 900:
+			return 2
+
+		if self.vel_x > 5:
+			self.vel_x = 5;
+		if self.vel_x < -5:
+			self.vel_x = -5
+
+		if self.vel_y > 5:
+			self.vel_y = 5;
+		if self.vel_y < -5:
+			self.vel_y = -5
+
+		self.x += self.vel_x
+		self.y += self.vel_y
+
+		return False
--- cleangame.py
+++ cleangame.py
@@ -1,4 +1,3 @@
-
 import pygame
 from pygame.locals import *
 
@@ -12,7 +11,7 @@ titre_fenetre="PongGame"
 
 pygame.init()
 
-#Ouverture de la fenÃªtre Pygame
+#Ouverture de la fenêtre Pygame
 fenetre = pygame.display.set_mode((900, 500))
 #Icone
 icone = pygame.image.load(image_ball)
@@ -26,10 +25,14 @@ continuer = 1
 
 playeur1 = Playeur(1)
 playeur2 = Playeur(2)
+ball = Ball(450,250)
 
-#Chargement et affichage de l'Ã©cran d'accueil
+#Chargement et affichage de l'écran d'accueil
 accueil = pygame.image.load(image_fond).convert()
 accueil = pygame.transform.scale(accueil, (900,500))
+
+font = pygame.font.SysFont("Comic Sans MS", 20)
+
 while continuer:	
 
 	#Rafraichissement
@@ -38,23 +41,75 @@ while continuer:
 	#Limitation de vitesse de la boucle
 	pygame.time.Clock().tick(120)
 
+	colliBall=ball.move(playeur1,playeur2)
+	if colliBall == 1:
+		ball = Ball(450,250)
+		playeur2.score += 1
+	if colliBall == 2:
+		ball = Ball(450,250)
+		playeur1.score += 1
+
 	for event in pygame.event.get():
-		#Si l'utilisateur quitte variable gÃ©nÃ©rale Ã  0 pour fermer la fenÃªtre
+		#Si l'utilisateur quitte variable générale à 0 pour fermer la fenêtre
 		if event.type == QUIT:
 			continuer_jeu = 0
 			continuer = 0
-		elif event.type == KEYDOWN:
-			#Si l'utilisateur presse Echap ici, on revient seulement au menu
-			if event.key == K_ESCAPE:
-				continuer_jeu = 0
-				continuer = 0
-			#Touches de dÃ©placement
-			elif event.key == K_UP:
-				playeur1.moveKeyboard(1)
-			elif event.key == K_DOWN:
-				playeur1.moveKeyboard(-1)
+			
+	keys = pygame.key.get_pressed()
+	#Si l'utilisateur presse Echap ici, on revient seulement au menu
+	if keys[pygame.K_ESCAPE]:
+		continuer_jeu = 0
+		continuer = 0
 
+	if keys[pygame.K_n] or keys[pygame.K_b]:
+		playeur1 = Playeur(1)
+		playeur2 = Playeur(2)
+		ball = Ball(450,250)
+		if keys[pygame.K_b]:
+			playeur1.bot = True
+	#Touches de déplacement
+	if keys[pygame.K_UP]:
+		playeur2.moveKeyboard(-1)
+	if keys[pygame.K_DOWN]:
+		playeur2.moveKeyboard(1)
+
+	if playeur1.bot == True and ball.x < 200 and ball.x > playeur1.x + playeur1.width:
+		if ball.y > playeur1.y+playeur1.height/2:
+			playeur1.moveKeyboard(1)
+		if ball.y < playeur1.y+playeur1.height/2:
+			playeur1.moveKeyboard(-1)
+	else:
+		if keys[pygame.K_w]:
+			playeur1.moveKeyboard(-1)
+		if keys[pygame.K_s]:
+			playeur1.moveKeyboard(1)
 
 	fenetre.blit(accueil, (0,0))
+	if playeur1.bot == True:
+		bot = font.render("bot", 1, (0,0,0))
+		fenetre.blit(bot, (290,200))
+	else:
+		playeur1u = font.render("/\\ Z", 1, (0,0,0))
+		fenetre.blit(playeur1u, (280,200))
+		playeur1d = font.render("\\/ S", 1, (0,0,0))
+		fenetre.blit(playeur1d, (280,260))
+	score1 = font.render(str(playeur1.score), 1, (0,0,0))
+	fenetre.blit(score1, (275,230))
+
+	playeur1u = font.render("/\\ up arrow", 1, (0,0,0))
+	fenetre.blit(playeur1u, (575,200))
+	playeur1d = font.render("\\/ down arrow", 1, (0,0,0))
+	fenetre.blit(playeur1d, (575,260))
+	score2 = font.render(str(playeur2.score), 1, (0,0,0))
+	fenetre.blit(score2, (580, 230))
+
+	info1 = font.render("Press N - New game", 1, (0,0,0))
+	fenetre.blit(info1, (350, 410))
+	info2 = font.render("Press ecape - Quit the game", 1, (0,0,0))
+	fenetre.blit(info2, (350, 440))
+	info3 = font.render("Press B - Bot vs Playeur", 1, (0,0,0))
+	fenetre.blit(info3, (350, 470))
+
 	fenetre.blit(playeur1.sprite, (playeur1.x, playeur1.y))
 	fenetre.blit(playeur2.sprite, (playeur2.x, playeur2.y))
+	fenetre.blit(ball.sprite, (ball.x, ball.y))
